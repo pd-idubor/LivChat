4bec0d7a690498bebc36f2d18dcebdb422b52057
Precious Idubor <preciousidubor@gmail.com>
2023-10-04 08:55:44 -0700

    Front end test

--- frontend-js/main.js
+++ frontend-js/main.js
@@ -2,28 +2,28 @@ import sth from './server.js';
 const activeUsers = new Set();
 const data = req.userId;
 
-//Basic functionalities
+//Functionalities
 
-io.on("connection", function (socket) {
-  console.log("Made socket connection...");
+io.on('connection', function (socket) {
+  console.log('Made socket connection...');
 
-  socket.on("new user", function (data) {
+  socket.on('new user', function (data) {
     socket.userId = data;
     activeUsers.add(data);
-    io.emit("new user", [...activeUsers]);
+    io.emit('new user', [...activeUsers]);
   });
 
-  socket.on("disconnect", () => {
+  socket.on('disconnect', () => {
     activeUsers.delete(socket.userId);
-    io.emit("user is inactive", socket.userId);
+    io.emit('user is inactive', socket.userId);
   });
 
 //Broadcast user message
-  socket.on("chat message", function (data) {
-    io.emit("chat message", data);
+  socket.on('chat message', function (data) {
+    io.emit('chat message', data);
   });
 
-  socket.on("typing", function (data) {
-    socket.broadcast.emit("typing", data);
+  socket.on('typing', function (data) {
+    socket.broadcast.emit('typing', data);
   });
 });
