18cbc7d53f988123397e9a37e086e849021b918a
pd-idubor <preciousidubor@gmail.com>
2024-08-30 14:25:14 +0100

    Moved some styles to css file and automated follow function with slight error check modification

--- public/js/follow.js
+++ public/js/follow.js
@@ -1,28 +1,30 @@
-let state = document.getElementById("folButton");
-state.addEventListener("click", function(e) {
-    console.log("Button: " + "clicked");
-    const value = e.target.dataset.follow;
-    console.log("The value of dataset: ", value);
-    const [
-        user,
-        fellow,
-        action
-    ] = value.split(';');
+(function() {
+    let state = document.getElementById("folButton");
+    if (!state) {
+        return;
+    }
+    state.addEventListener("click", function(e) {
+        const value = e.target.dataset.follow;
+        const [
+            user,
+            fellow,
+            action
+        ] = value.split(';');
 
-    console.log("From the button click: ", user, fellow, action);
-    let data = { follower: user, following: fellow, action: action };
-    console.log("This is the data to be sent: ", data);
+        let data = { follower: user, following: fellow, action: action };
+        (async() => {
+            const rawResponse = fetch('/follow', {
+                method: "POST",
+                headers: { 'Accept': 'application/json', 'Content-Type': 'application/json' },
+                body: JSON.stringify(data)
+            });
+            const content = await rawResponse;
 
-    (async() => {
-        const rawResponse = fetch('/follow', {
-            method: "POST",
-            headers: { 'Accept': 'application/json', 'Content-Type': 'application/json' },
-            body: JSON.stringify(data)
-        });
-        const content = await rawResponse;
+            if (!content) {
+                return (console.log("An error occured."));
+            };
 
-        console.log(content);
-
-        location.reload(true);
-    })();
-});
+            location.reload(true);
+        })();
+    });
+})();
